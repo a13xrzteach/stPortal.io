df1a8c7048c2ce22343d4efc32686d842117d1ed
a13xrzteach <132148068+a13xrzteach@users.noreply.github.com>
2024-12-04 13:58:59 -0500

    "TA" link updated

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // Example: Redirect to another page when a button is clicked
 document.getElementById('button1').addEventListener('click', function () {
-  window.location.href = 'grades.html';
+  window.location.href = 'http://10.242.207.207:5001';
 });
 
 // Repeat for other buttons
