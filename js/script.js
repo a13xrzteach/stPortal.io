cc6b67192831205eb8f9248af44078f9e897a5e4
a13xrzteach <132148068+a13xrzteach@users.noreply.github.com>
2024-12-04 10:27:41 -0500

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Example: Redirect to another page when a button is clicked
+document.getElementById('button1').addEventListener('click', function () {
+  window.location.href = 'grades.html';
+});
+
+// Repeat for other buttons
