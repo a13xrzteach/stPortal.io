80884d374fce886b0e2b2fb08a1c702b9dd5baec
a13xrzteach <132148068+a13xrzteach@users.noreply.github.com>
2025-01-08 08:34:59 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,4 +7,8 @@ document.getElementById('button2').addEventListener('click', function () {
   window.location.href = 'staff.html';
 });
 
+document.getElementById('button3').addEventListener('click', function () {
+  window.location.href = 'guidance-chat.html';
+});
+
 // Repeat for other buttons
